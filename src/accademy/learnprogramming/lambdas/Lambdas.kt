66aa98170a0965f0add5ce39f8c88b70cb36d524
Vkashaykova <vkashaykova@gmail.com>
2024-06-07 12:23:16 +0300

    commit lambda with receivers

--- src/accademy/learnprogramming/lambdas/Lambdas.kt
+++ src/accademy/learnprogramming/lambdas/Lambdas.kt
@@ -11,26 +11,59 @@ fun main(args: Array<String>) {
         Employee("Mike", "Jones", 2002)
     )
 
-    //println(employees.minBy(Employee::startYear))
+    findByLastName(employees, "Wilson")
+    findByLastName(employees, "Smithson")
 
-//    var num = 10
-//    run {
-//        num += 15
-//        println(num)
-//    }
-
-    run(::topLevel)
-
-}
-
-fun topLevel() = println("I'm in a function!")
-
-fun useParameter(employees: List<Employee>, num: Int) {
-    employees.forEach {
-        println(it.firstName)
-        println(num)
+    "Some String".apply somestring@{
+        "Another String".apply {
+            println(toLowerCase())
+            println(this@somestring.toUpperCase())
+        }
     }
+
+
 }
+
+fun findByLastName(employees: List<Employee>, lastName: String) {
+    employees.forEach returnBlock@{
+        if (it.lastName == lastName) {
+            println("Yes, there's an employee with the last name $lastName")
+            return@returnBlock
+        }
+    }
+
+    println("Nope, there's no employee with the last name $lastName")
+}
+
+fun countTo100() =
+    StringBuilder().apply() {
+        for (i in 1..99) {
+            append(i)
+            append(", ")
+        }
+        append(100)
+    }.toString()
+
+//fun countTo100() =
+//        with(StringBuilder()) {
+//            for (i in 1..99) {
+//                append(i)
+//                append(", ")
+//            }
+//            append(100)
+//            toString()
+//        }
+
+
+//fun countTo100(): String {
+//    val numbers = StringBuilder()
+//    for (i in 1..99) {
+//        numbers.append(i)
+//        numbers.append(", ")
+//    }
+//    numbers.append(100)
+//    return numbers.toString()
+//}
 
 data class Employee(val firstName: String, val lastName: String, val startYear: Int) {
 
